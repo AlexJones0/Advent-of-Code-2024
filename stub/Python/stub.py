6e3a60a17182397b7a887a2b94da3654cc712f17
Alex Jones <alex.jones@lowrisc.org>
2024-12-01 19:10:24 +0000

    Add python+rust stubs

---
+++ stub/Python/stub.py
@@ -0,0 +1,12 @@
+"""
+FILE: Day XX/Python/sol.py
+Author: Alex Jones
+Desc: Solution to day X problems (X & X) for Advent of Code 2024, solved in Python 3.
+"""
+NOT_IMPLEMENTED = "Not Yet Implemented"
+data = open("Day XX/data.txt", "r").read().replace("\r","").split("\n")
+
+
+print("Problem X:", NOT_IMPLEMENTED)
+
+print("Problem X:", NOT_IMPLEMENTED)
