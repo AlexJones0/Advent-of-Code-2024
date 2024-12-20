123bc73f2258d8901aebda72dccd8437a2e50c9a
Alex Jones <alex.jones@lowrisc.org>
2024-12-20 23:03:31 +0000

    Optimise day 6 part 2 solution (10x speedup)
    My part 2 solution already contained 2 optimisations: (1) pre-compute a "visited" path in part 1 and only try placing obstacles on nodes explored in our original path, and (2) restore simulations from just before the point of collision.
    
    I add a third optimisation which results in around 10x speedup. For each (pos, dir) we visit, we compute and memoize the location of the next obstacle (or grid boundary) it hits. When we place an obstacle in part 2, we always progress to the less boundary, unless the obstacle was placed between our current position and the next position - at which point we instead move to just before the obstacle. This allows us to memoize long repeated iterations of walking, which happens a lot as cycles on the path tend to form in the same place. Hence cutting runtime by a further 10x or so (around 0.3 seconds on Python and milliseconds on Rust)

--- Day 06/Python/sol.py
+++ Day 06/Python/sol.py
@@ -3,7 +3,6 @@ FILE: Day 06/Python/sol.py
 Author: Alex Jones
 Desc: Solution to day 6 problems (11 & 12) for Advent of Code 2024, solved in Python 3.
 """
-NOT_IMPLEMENTED = "Not Yet Implemented"
 data = open("Day 06/data.txt", "r").read().strip().replace("\r","").split("\n")
 
 turn_right = {(-1,0): (0,1), (0,1): (1,0), (1,0): (0,-1), (0,-1): (-1,0)}
@@ -27,13 +26,47 @@ path = []
 simulate(grid_map, pos, (-1,0), path)
 print("Problem 11:", len(set(p for (p, _) in path)))
 
+from functools import cache
+
+@cache
+def find_next_collision(pos, dir_):
+    while pos in grid_map and grid_map[pos] != '#':
+        pos = (pos[0] + dir_[0], pos[1] + dir_[1])
+    return pos
+
+def between(item, one, two):
+    return one <= item < two or two <= item < one
+
+def new_obstacle_blocks_path(current_pos, current_dir, new_pos, obstacle):
+    if current_dir[0] != 0 and current_pos[1] == obstacle[1]:
+        return between(obstacle[0], current_pos[0], new_pos[0])
+    elif current_dir[1] != 0 and current_pos[0] == obstacle[0]:
+        return between(obstacle[1], current_pos[1], new_pos[1])
+    return False
+
 ans = 0
-visited = set([])
-# Opt 1: Only need to check placing obstacles on our path
+visited_on_path = set()
+# Optimisation 1: Only try placing obstacles on nodes explored in our original path
 for i, ((prev_pos, prev_dir), (pos, _)) in enumerate(zip(path[:-1], path[1:])):
     # Only check each position once, the first time encountered
-    if pos == prev_pos or pos in visited: continue
-    visited.add(pos)
-    # Opt 2: simulate restoring from just before the point of collision
-    ans += not simulate(grid_map | {pos: '#'}, prev_pos, prev_dir, path[:i])
+    if pos == prev_pos or pos in visited_on_path: continue
+    visited_on_path.add(pos)
+    # Optimisation 2: Simulate restoring from just before the point of collision
+    current_pos, next_pos, current_dir = prev_pos, prev_pos, prev_dir
+    seen = set()
+    while next_pos in grid_map and (current_pos, current_dir) not in seen:
+        seen.add((current_pos, current_dir))
+        # Optimisation 3: Use a memoized global result to automatically find the next
+        # collision given the current position and direction. Then, just check if the
+        # newly inserted obstacle would have been on that collision path - if so, set
+        # the new position to be just before the obstacle instead.
+        next_pos = find_next_collision(current_pos, current_dir)
+        if new_obstacle_blocks_path(current_pos, current_dir, next_pos, pos):
+            next_pos = pos
+        current_pos = (next_pos[0] - current_dir[0], next_pos[1] - current_dir[1])
+        current_dir = turn_right[current_dir]
+    # Only increment if we stopped searching because we found a cycle, instead of 
+    # finding the grid border
+    ans += next_pos in grid_map
+
 print("Problem 12:", ans)
--- Day 06/Rust/sol.rs
+++ Day 06/Rust/sol.rs
@@ -3,6 +3,8 @@
  * Author: Alex Jones
  * Desc: Solution to day 6 problems (11 & 12) for Advent of Code 2024, solved in Rust.
  */
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs;
 
@@ -46,6 +48,45 @@ pub fn simulate(
     !seen.contains(&(pos, dir))
 }
 
+type Memo = HashMap<State, (i32, i32)>;
+fn find_next_collision(
+    memo: &mut Memo,
+    dims: (usize, usize),
+    obstacles: &HashSet<(usize, usize)>,
+    mut pos: (i32, i32),
+    dir: (i32, i32),
+) -> (i32, i32) {
+    let entry = memo.entry((pos, dir));
+    if let Entry::Occupied(e) = entry {
+        *e.get()
+    } else {
+        while pos_in_bounds(dims, pos) && !obstacles.contains(&(pos.0 as usize, pos.1 as usize)) {
+            pos = (pos.0 + dir.0, pos.1 + dir.1);
+        }
+        entry.or_insert(pos);
+        pos
+    }
+}
+
+fn between(item: i32, one: i32, two: i32) -> bool {
+    one <= item && item < two || two <= item && item < one
+}
+
+fn new_obstacle_blocks_path(
+    current_pos: (i32, i32),
+    current_dir: (i32, i32),
+    new_pos: (i32, i32),
+    obstacle: (i32, i32),
+) -> bool {
+    if current_dir.0 != 0 && current_pos.1 == obstacle.1 {
+        between(obstacle.0, current_pos.0, new_pos.0)
+    } else if current_dir.1 != 0 && current_pos.0 == obstacle.0 {
+        between(obstacle.1, current_pos.1, new_pos.1)
+    } else {
+        false
+    }
+}
+
 pub fn solve() {
     let contents: String = fs::read_to_string("Day 06/data.txt")
         .expect("Could not read data file")
@@ -77,23 +118,40 @@ pub fn solve() {
     ));
     println!("Problem 11: {}", path.iter().map(|x| x.0).unique().count());
 
+    let mut memo = Memo::new();
+
     let mut ans = 0u32;
-    let mut visited = HashSet::<(i32, i32)>::new();
-    // Opt 1: Only ned to check placing obstacles on our path
+    let mut visited_on_path = HashSet::new();
+    // Optimisation 1: Only try placing obstacles on nodes explored in our original path
     for i in 1..path.len() {
         let pos = path[i].0;
         let (prev_pos, prev_dir) = path[i - 1];
         // Only check each position once, the first time encountered
-        if pos == prev_pos || visited.contains(&pos) {
+        if pos == prev_pos || visited_on_path.contains(&pos) {
             continue;
         }
-        visited.insert(pos);
-        // Opt 2: simulate restoring from just before the point of collision
-        let mut new_obstacles = obstacles.clone();
-        new_obstacles.insert((pos.0 as usize, pos.1 as usize));
-        let mut prev_path = vec![((0, 0), (0, 0)); i - 1];
-        prev_path.clone_from_slice(&path[..(i - 1)]);
-        ans += !simulate(dims, &new_obstacles, prev_pos, prev_dir, &mut prev_path) as u32;
+        visited_on_path.insert(pos);
+        // Optimisation 2: Restore simulation from just before the point of collision
+        let mut current_pos = prev_pos;
+        let mut next_pos = prev_pos;
+        let mut current_dir = prev_dir;
+        let mut seen = HashSet::new();
+        while pos_in_bounds(dims, next_pos) && !seen.contains(&(current_pos, current_dir)) {
+            seen.insert((current_pos, current_dir));
+            // Optimisation 3: Use a memoized global result to automatically find the
+            // next collision given the current posiition and direction. Then, just check
+            // if the newly inserted obstacle would have been on that collision path - if
+            // so, set the new position to be just before the obstacle instead.
+            next_pos = find_next_collision(&mut memo, dims, &obstacles, current_pos, current_dir);
+            if new_obstacle_blocks_path(current_pos, current_dir, next_pos, pos) {
+                next_pos = pos;
+            }
+            current_pos = (next_pos.0 - current_dir.0, next_pos.1 - current_dir.1);
+            current_dir = turn_right(current_dir);
+        }
+        // Only increment if we stop searching because we found a cycle, instead
+        // of finding the grid border.
+        ans += pos_in_bounds(dims, next_pos) as u32;
     }
     println!("Problem 12: {}", ans);
 }
